9293be6a3fd0c8230af7e1b12446696e2696839b
nguyenducsang811 <nguyenducsang08112003@gmail.com>
2026-05-08 00:59:01 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+        base: '/SO-N-GI-O-N-N-NG-L-C-S-/',
       server: {
         port: 3000,
         host: '0.0.0.0',
